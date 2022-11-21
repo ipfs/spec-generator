83fc09575a54e749f961e6b911a29a5a4e3cb5cb
Robin Berjon <robin.berjon@nytimes.com>
2022-11-21 18:26:31 -0500

    port all of that test file

--- test-suite/ipfs/gateways/blocks.js
+++ test-suite/ipfs/gateways/blocks.js
@@ -1,18 +1,19 @@
 
-import { readFileSync } from "node:fs";
+import { readFile, stat } from "node:fs/promises";
 import { Buffer } from "node:buffer";
 import { env } from "node:process";
-import { ok, equal } from 'node:assert';
+import { ok, equal, match } from 'node:assert';
 import axios from "axios";
 import makeRel from '../../../lib/rel.js';
 
 const rel = makeRel(import.meta.url);
+const IPLD_RAW_TYPE = 'application/vnd.ipld.raw';
 const ROOT_GATEWAY_URL = env.ROOT_GATEWAY_URL;
-const dirAsRawBlock = readFileSync(rel('./fixtures/dir.expected.bin'));
+const dirAsRawBlock = await readFile(rel('./fixtures/dir.expected.bin'));
 // ipfs add -Qrw --cid-version 1 dir
 const ROOT_DIR_CID = 'bafybeie72edlprgtlwwctzljf6gkn2wnlrddqjbkxo3jomh4n7omwblxly';
 // ipfs resolve -r /ipfs/$ROOT_DIR_CID/dir/ascii.txt | cut -d "/" -f3
-// const FILE_CID = 'bafkreihhpc5y2pqvl5rbe5uuyhqjouybfs3rvlmisccgzue2kkt5zq6upq';
+const FILE_CID = 'bafkreihhpc5y2pqvl5rbe5uuyhqjouybfs3rvlmisccgzue2kkt5zq6upq';
 
 // XXX get the root URL
 describe('Test HTTP Gateway Raw Block (application/vnd.ipld.raw) Support', () => {
@@ -26,7 +27,7 @@ describe('Test HTTP Gateway Raw Block (application/vnd.ipld.raw) Support', () =>
   before('Writing a raw block to the gateway', async () => {
     const res = await ax.post('/ipfs/', {
       data: dirAsRawBlock,
-      headers: { 'Content-Type': 'application/vnd.ipld.raw' },
+      headers: { 'Content-Type': IPLD_RAW_TYPE },
       maxRedirects: 0,
     });
     ok(res.status < 400, 'Writing fixture was successful');
@@ -43,7 +44,7 @@ describe('Test HTTP Gateway Raw Block (application/vnd.ipld.raw) Support', () =>
     });
     it('GET for application/vnd.ipld.raw returns a raw block', async () => {
       const res = await ax.get(`/ipfs/${ROOT_DIR_CID}/dir`, {
-        headers: { Accept: 'application/vnd.ipld.raw' },
+        headers: { Accept: IPLD_RAW_TYPE },
         responseType: 'arraybuffer',
       });
       equal(res.status, 200, 'GET succeeded');
@@ -51,47 +52,50 @@ describe('Test HTTP Gateway Raw Block (application/vnd.ipld.raw) Support', () =>
       ok(bf.equals(dirAsRawBlock), 'received raw buffer is correct');
     });
   });
+
+  describe('Make sure expected HTTP headers are returned with the block bytes', () => {
+    it('GET response for application/vnd.ipld.raw has expected response headers', async () => {
+      const res = await ax.get(`/ipfs/${ROOT_DIR_CID}/dir/ascii.txt`, {
+        headers: { Accept: IPLD_RAW_TYPE },
+        responseType: 'arraybuffer',
+      });
+      equal(res.status, 200, 'GET succeeded');
+      equal(res.headers['content-type'], IPLD_RAW_TYPE, 'correct Content-Type');
+      const { size } = await stat(rel('./fixtures/dir/ascii.txt'));
+      equal(res.headers['content-length'], size, 'correct Content-Length');
+      match(res.headers['content-disposition'], new RegExp(`attachment;\\s*filename="${FILE_CID}\\.bin"`), 'correct Content-Disposition');
+      equal(res.headers['x-content-type-options'], 'nosniff', 'correct X-Content-Type-Options');
+    });
+    it('GET for application/vnd.ipld.raw with query filename includes Content-Disposition with custom filename', async () => {
+      const res = await ax.get(`/ipfs/${ROOT_DIR_CID}/dir/ascii.txt?filename=foobar.bin`, {
+        headers: { Accept: IPLD_RAW_TYPE },
+        responseType: 'arraybuffer',
+      });
+      equal(res.status, 200, 'GET succeeded');
+      match(res.headers['content-disposition'], new RegExp(`attachment;\\s*filename="foobar\\.bin"`), 'correct Content-Disposition');
+    });
+    // (basic checks, detailed behavior is tested in t0116-gateway-cache.sh)
+    it('GET response for application/vnd.ipld.raw has expected caching headers', async () => {
+      const res = await ax.get(`/ipfs/${ROOT_DIR_CID}/dir/ascii.txt`, {
+        headers: { Accept: IPLD_RAW_TYPE },
+        responseType: 'arraybuffer',
+      });
+      equal(res.status, 200, 'GET succeeded');
+      equal(res.headers.etag, `"${FILE_CID}.raw"`, 'correct Etag');
+      ok(res.headers['x-ipfs-path'], 'has X-Ipfs-Path');
+      ok(res.headers['x-ipfs-roots'], 'has X-Ipfs-Roots');
+      const cachePragmata = (res.headers['cache-control'] || '').split(/\s*,\s*/);
+      Object.entries({
+          'public pragma': (str) => str.toLowerCase() === 'public',
+          'immutable pragma': (str) => str.toLowerCase() === 'immutable',
+          'max-age pragma': (str) => {
+            if (!/^max-age=/i.test(str)) return false;
+            if (parseInt(str.replace('max-age=', ''), 10) < 29030400) return false; // at least 29030400
+            return true;
+          },
+        })
+        .forEach(([label, func]) => ok(cachePragmata.find(func), label))
+      ;
+    });
+  });
 });
-
-// # Make sure expected HTTP headers are returned with the block bytes
-
-//     test_expect_success "GET response for application/vnd.ipld.raw has expected Content-Type" '
-//     curl -svX GET -H "Accept: application/vnd.ipld.raw" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT_DIR_CID/dir/ascii.txt" >/dev/null 2>curl_output &&
-//     cat curl_output &&
-//     grep "< Content-Type: application/vnd.ipld.raw" curl_output
-//     '
-
-//     test_expect_success "GET response for application/vnd.ipld.raw includes Content-Length" '
-//     BYTES=$(ipfs block get $FILE_CID | wc --bytes)
-//     grep "< Content-Length: $BYTES" curl_output
-//     '
-
-//     test_expect_success "GET response for application/vnd.ipld.raw includes Content-Disposition" '
-//     grep "< Content-Disposition: attachment\; filename=\"${FILE_CID}.bin\"" curl_output
-//     '
-
-//     test_expect_success "GET response for application/vnd.ipld.raw includes nosniff hint" '
-//     grep "< X-Content-Type-Options: nosniff" curl_output
-//     '
-
-//     test_expect_success "GET for application/vnd.ipld.raw with query filename includes Content-Disposition with custom filename" '
-//     curl -svX GET -H "Accept: application/vnd.ipld.raw" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT_DIR_CID/dir/ascii.txt?filename=foobar.bin" >/dev/null 2>curl_output_filename &&
-//     cat curl_output_filename &&
-//     grep "< Content-Disposition: attachment\; filename=\"foobar.bin\"" curl_output_filename
-//     '
-
-// # Cache control HTTP headers
-// # (basic checks, detailed behavior is tested in  t0116-gateway-cache.sh)
-
-//     test_expect_success "GET response for application/vnd.ipld.raw includes Etag" '
-//     grep "< Etag: \"${FILE_CID}.raw\"" curl_output
-//     '
-
-//     test_expect_success "GET response for application/vnd.ipld.raw includes X-Ipfs-Path and X-Ipfs-Roots" '
-//     grep "< X-Ipfs-Path" curl_output &&
-//     grep "< X-Ipfs-Roots" curl_output
-//     '
-
-//     test_expect_success "GET response for application/vnd.ipld.raw includes Cache-Control" '
-//     grep "< Cache-Control: public, max-age=29030400, immutable" curl_output
-//     '
