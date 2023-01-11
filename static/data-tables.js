d440c1c055a2eed75b5b8bcd4e85bb3b521f7060
Robin Berjon <robin.berjon@nytimes.com>
2023-01-11 18:21:51 -0500

    make the table sort and all

--- static/data-tables.js
+++ static/data-tables.js
@@ -1,2 +1,32 @@
 
-// XXX
+Array.from(document.querySelectorAll('table.ipseity-data-table'))
+  .forEach(table => {
+    Array.from(table.querySelectorAll('thead th')).forEach((th, idx) => {
+      th.addEventListener('click', makeTableSorter(table, th, idx));
+    });
+  })
+;
+
+function makeTableSorter (table, th, idx) {
+  // parseInt() without radix is on purpose, we get 0x prefixes
+  const sortFunctionsByType = {
+    string: (a, b) => (a || '').localeCompare(b),
+    int: (a, b) => {
+      if (parseInt(a) < parseInt(b)) return -1;
+      if (parseInt(a) > parseInt(b)) return 1;
+      return 0;
+    },
+  };
+  const sort = sortFunctionsByType[th.getAttribute('data-ipseity-sort')] || sortFunctionsByType.string;
+  return () => {
+    const tbody = table.querySelector('tbody');
+    const rows = Array.from(tbody.querySelectorAll('tr'))
+      .sort((a, b) => {
+        const valA = a.querySelector(`td:nth-of-type(${idx + 1})`).textContent;
+        const valB = b.querySelector(`td:nth-of-type(${idx + 1})`).textContent;
+        return sort(valA, valB);
+      })
+    ;
+    tbody.replaceChildren(...rows);
+  };
+}
